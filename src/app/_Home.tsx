ef5b2cf7b4544080a6db63d3f962cc2bca8e47f8
Tyler J. Cagle <tylerjcagle@gmail.com>
2024-07-07 21:53:27 -0400

    try something

--- src/app/_Home.tsx
+++ src/app/_Home.tsx
@@ -3,16 +3,19 @@
 import dynamic from "next/dynamic";
 import { useEffect, useState } from "react";
 
-const Main = dynamic(() => import("./_Main"), { ssr: false });
-
 const _ = () => {
   const [loaded, setLoaded] = useState(false);
+
+  const render = () => {
+    const Main = dynamic(() => import("./_Main"), { ssr: false });
+    return <Main />;
+  };
 
   useEffect(() => {
     setLoaded(true);
   }, []);
 
-  return loaded ? <Main /> : null;
+  return loaded ? render() : null;
 };
 
 export default _;
